8f3f8c607461af625351a62cb193ab4d382bbef6
Sergey Anosov <106556264+SergeyAnosov@users.noreply.github.com>
2022-10-11 12:30:17 +0300

    Update ValidationException.java

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ValidationException extends RuntimeException {
     public ValidationException(String s) {
         super(s);
